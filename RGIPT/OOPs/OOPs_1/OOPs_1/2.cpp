dc7ddf5f4ef636e4da82e688cdeedb46b5ec18ab
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2025-08-13 22:40:28 +0530

    Create 2.cpp

---
+++ RGIPT/OOPs/OOPs_1/OOPs_1/2.cpp
@@ -0,0 +1,15 @@
+// Write a program in C++ to Arithmetic operations of two integer numbers.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+    cout << "Sum: " << num1 + num2 << endl;
+    cout << "Difference: " << num1 - num2 << endl;
+    cout << "Product: " << num1 * num2 << endl;
+    cout << "Quotient: " << num1 / num2 << endl;
+    cout << "Remainder: " << num1 % num2 << endl;
+    return 0;
+}
