6a77b03ce87a699188f6bc4f33ab8da5bc00d2bd
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2025-08-13 22:41:58 +0530

    Create 10.cpp

---
+++ RGIPT/OOPs/OOPs_1/OOPs_1/10.cpp
@@ -0,0 +1,15 @@
+// Write a program in C++ to find the smallest of 10 numbers.
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int smallest = INT_MAX, num;
+    cout << "Enter 10 numbers: ";
+    for (int i = 0; i < 10; i++) {
+        cin >> num;
+        if (num < smallest) smallest = num;
+    }
+    cout << "Smallest number: " << smallest << endl;
+    return 0;
+}
